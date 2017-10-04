143a880e24abdfb698c254cc42352c69538658b2
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2017-10-04 14:45:41 +0200

    histogramme pour la décomposition + fichier sinus

---
+++ TP1_visu/script_python.py
@@ -0,0 +1,13 @@
+import numpy as math
+import numpy as np
+
+#0.1
+
+abscisse = np.linspace(-1.0, 1.0, num=2000)
+
+mon_fichier = open("fichierSinus.txt", "w")
+
+for x in abscisse:
+    mon_fichier.write(str(math.sin(x*math.pi)))
+    mon_fichier.write("\n")
+mon_fichier.close()
