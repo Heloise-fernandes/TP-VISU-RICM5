d37fa5c6abbf196a0705d467fc2255fef9dcb8a6
alice2909 <alice.rivoal@gmail.com>
2017-10-06 15:10:03 +0200

    erreur graph

---
+++ TP1_visu/erreur_graph.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	function requesterreurs()
+	{
+		var detail = 1;
+		for (var i =0; i <= 10; i++){
+			detail = detail / 10;
+			$.get('deconstruction.php?file=fichierSinus.txt&NIvDetail=detail',function(data){
+				var resdec = JSON.parse(data);
+				$.get('reconstruction.php?decompo=resdec[\'decompo\']', function(data){
+					var resrec = JSON.parse(data);
+					$.get('erreur.php?recompo=resrec[\'recompo\']&origin=resdec[\'origin\']',function(data){
+						var err = JSON.parse(data);
+						nivDetail[i] = detail;
+						erreurs[i] = err['erreur_quadra'];
+					});
+				});
+			});
+		};
+
+		var trace = {
+			x: nivDetail,
+			y: erreurs[i],
+			type:'scatter'
+		};
+		Plotly.newPlot('Erreurs en fonction du niveau de détail', [trace]);
+
+
+	};
+
+
+	requesterreurs();
+});
