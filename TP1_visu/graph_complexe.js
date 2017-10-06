5a07e26dae98d4c70712dc64e5f7c54ef9da5c6a
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2017-10-06 14:33:26 +0200

    Sinus ok

--- TP1_visu/graph_complexe.js
+++ TP1_visu/graph_complexe.js
@@ -1,12 +1,32 @@
 $(document).ready(function() {
 
-	function requestDecompose(tabAbscisse)
+	function requestGetOrigine(tabAbscisse)
 	{
-
-
 		$.get('deconstruction.php?file=fichierSinus.txt',function(data){
 		 		var res = JSON.parse(data);
+		 		//console.log(data);
+	            //res['origin'];
+	            //res['decompo'];
+				
+	            var trace1 = {
+				  x: tabAbscisse,
+				  y: res['origin'],
+				  type: 'scatter'
+				};
 
+				var data = [trace1];
+
+				Plotly.newPlot('tabOrigine', data);
+
+		});
+
+	}
+
+	function requestdecomporeconstr(tabAbscisse)
+	{
+		$.get('deconstruction.php?file=fichierSinus.txt',function(data){
+		 		var res = JSON.parse(data);
+		 		//console.log(data);
 	            //res['origin'];
 	            //res['decompo'];
 				
@@ -27,15 +47,15 @@ $(document).ready(function() {
 	//-1.0, 1.0, num=2000
 
 	var x = [];
-	var offset = 2/2000;
+	var offset = 2/2048;
 	var value = -1;
 	for (var i = 0; i <2000; i++) {
 		x[i]= value
 		value+=offset;
 	}
 
-	console.log(offset);
-	console.log(x);
+	requestGetOrigine(x)
+	requestdecomporeconstr(x)
 
 
 	//requestDecompose();
