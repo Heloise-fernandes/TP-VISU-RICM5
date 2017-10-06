89f2066e61523b327e95ca79ee6419bd381e9c1c
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2017-10-06 14:45:09 +0200

    modification reconstruction et suite test 3 et 4

--- TP1_visu/graph_complexe.js
+++ TP1_visu/graph_complexe.js
@@ -24,27 +24,42 @@ $(document).ready(function() {
 
 	function requestdecomporeconstr(tabAbscisse)
 	{
+
 		$.get('deconstruction.php?file=fichierSinus.txt',function(data){
 		 		var res = JSON.parse(data);
-		 		//console.log(data);
-	            //res['origin'];
-	            //res['decompo'];
-				
-	            var trace1 = {
-				  x: tabAbscisse,
-				  y: res['origin'],
-				  type: 'scatter'
-				};
 
-				var data = [trace1];
+				var decompo = res['decompo']
+				var about=escape( $("#editorAbout").text());
+			    $.ajax({
+			        url: "reconstruction.php",
+			        type:"post",
+			        async: false,
+			        data: {
+			            decompo: decompo
+			        },
+			        success: function(data){   
+			        	var res = JSON.parse(data);
+					
+			            var trace1 = {
+						  x: tabAbscisse,
+						  y: res['recompo'],
+						  type: 'scatter'
+						};
 
-				Plotly.newPlot('tabOrigine', data);
+						var data = [trace1];
 
+						Plotly.newPlot('tabReconstruct', data);  
+
+			        },
+			        error:function(xhr, ajaxOptions, thrownError){alert(xhr.responseText); ShowMessage("??? ?? ?????? ??????? ????","fail");}
+			    });
 		});
+
+		
 
 	}
 
-	//-1.0, 1.0, num=2000
+	//-1.0, 1.0, num=2048
 
 	var x = [];
 	var offset = 2/2048;
