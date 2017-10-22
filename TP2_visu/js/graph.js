e9d90ed932a9776983d15baf833a45b02396e2a3
douria <douria.zen@gmail.com>
2017-10-22 18:06:07 +0200

    version decomposition qui marche

--- TP2_visu/js/graph.js
+++ TP2_visu/js/graph.js
@@ -2,33 +2,34 @@ $(document).ready(function() {
 
 
 	//context.beginPath();
-	function drawImage(idCanvas,tableaux)
+	function drawImage(idCanvas,tableaux,coef)
 	{
 	    var canvas = document.getElementById(idCanvas);
 	    var contexte = canvas.getContext("2d");
 	    var h = canvas.height;
 	    var w = canvas.width;
-	    var coef = 20;
+	    //var coef = 20;
 
 	    
 	    var count = tableaux.length;
 
 	    contexte.beginPath();
-	    contexte.moveTo( tableaux[0].x *coef, (h - tableaux[0].y*coef));
+	    contexte.moveTo( (w/2 -tableaux[0].x *coef), (h/2 - tableaux[0].y*coef));
  		for(var i = 1; i < count; i++)
  		{
- 			contexte.lineTo( tableaux[i].x *coef, (h - tableaux[i].y*coef));
+ 			contexte.lineTo( (w/2 -tableaux[i].x *coef), (h/2 - tableaux[i].y*coef));
  		}
+ 		contexte.lineTo( (w/2 -tableaux[0].x *coef), (h/2 - tableaux[0].y*coef));
  		contexte.stroke();
 	}
 	
 	function displayCroc()
 	{
-		 $.get('../php/decomposition.php?file=../sources_files/sh.d',function(data){
+		 $.get('../php/decomposition.php?file=../sources_files/crocodile512.d',function(data){
 			var res = JSON.parse(data);
-
-			drawImage("canvas",res['moyenne']);
-			drawImage("canvasOrigin",res['origin']);
+			
+			drawImage("canvas",res['moyenne'],20);
+			drawImage("canvasOrigin",res['origin'],25);
 
 		});
 	}
--- TP2_visu/php/decomposition.php
+++ TP2_visu/php/decomposition.php
@@ -2,39 +2,68 @@
 require_once('./inputOutput.php');
 
 
-	function decomposition($tab)
+	function decomposition_totale($tab){
+		/* premiere decomposition du tableau*/
+		$details=[];
+		$res=[];
+		$res=decomposition($tab,$details);
+
+		while(sizeof($res[0]) > 4){
+			$res=decomposition($res[0],$res[1]);
+		}
+		 return $res;
+	}
+
+
+
+	function decomposition($tab,$details)
 	{
 		$tab_decompose=[];
-		$tab_details=[];
 		$count = sizeof($tab);
-        for ($i = 1; $i < $count; $i++) 
+		// la taille du tableau details (on rajoute les details au fur et a mesure
+		$tbs=sizeof($details);
+		/* on calcule les points a probleme afin d'eviter de faire le modulo*/
+		$tab_decompose[0]=['x' => (-$tab[$count-2]['x']+3.0*($tab[$count-1]['x']
+						+$tab[0]['x'])-$tab[1]['x'])/4.0, 
+						
+				'y' => (-$tab[$count-2]['y']+3.0*($tab[$count-1]['y']
+						+$tab[0]['y'])-$tab[1]['y'])/4.0 ];
+			//calcul des premiers details 
+			$details[$tbs]=['x' => floatval(($tab[$count-2]['x']+3*(-$tab[$count-1]['x']
+						+$tab[0]['x'])-$tab[1]['x'])/4.0), 
+					'y' => floatval(($tab[$count-2]['y']+3*(-$tab[$count-1]['y']
+						+$tab[0]['y'])-$tab[1]['y'])/4.0) ];	
+						
+        for ($i = 1; $i < $count/2; $i++) 
 		{
+			$tab_decompose[$i]=['x' => floatval(-$tab[2*$i-2]['x']+floatval(3.0*($tab[2*$i-1]['x']
+						+$tab[2*$i]['x']))-$tab[2*$i+1]['x'])/4.0, 
+						
+					'y' => floatval(-$tab[2*$i-2]['y']+floatval(3.0*($tab[2*$i-1]['y']
+						+$tab[2*$i]['y']))-$tab[2*$i+1]['y'])/4.0 ];
+			  		
+			$details[$i+$tbs]=['x' => floatval(($tab[2*$i-2]['x']+3*(-$tab[2*$i-1]['x']
+						+$tab[2*$i]['x'])-$tab[2*$i+1]['x'])/4.0), 
+			'y' => floatval(($tab[2*$i-2]['y']+3*(-$tab[2*$i-1]['y']
+						+$tab[2*$i]['y'])-$tab[2*$i+1]['y'])/4.0) ]; 
 			
-			$tab_decompose[$i]=['x' => floatval((1/4)*(-$tab[(2*$i-2)%$count]['x']+3*$tab[(2*$i-1)%$count]['x']
-						+3*$tab[(2*$i)%$count]['x']-$tab[(2*$i+1)%$count]['x'])), 
-			'y' => floatval((1/4)*(-$tab[(2*$i-2)%$count]['y']+3*$tab[(2*$i-1)%$count]['y']
-						+3*$tab[(2*$i)%$count]['y']-$tab[(2*$i+1)%$count]['y'])) ];
-
-			$tab_details[$i]=['x' => floatval((1/4)*($tab[(2*$i-2)%$count]['x']-3*$tab[(2*$i-1)%$count]['x']
-						+3*$tab[(2*$i)%$count]['x']-$tab[(2*$i+1)%$count]['x'])), 
-			'y' => floatval((1/4)*($tab[(2*$i-2)%$count]['y']-3*$tab[(2*$i-1)%$count]['y']
-						+3*$tab[(2*$i)%$count]['y']-$tab[(2*$i+1)%$count]['y'])) ]; 		
-
 		}
- 		return [$tab_details, $tab_decompose];
+ 		return [$tab_decompose ,$details];
 	}
 
 	if( isset($_GET["file"]) )
 	{
+		
 		$tab = read($_GET["file"]);
 	}
 	else
 	{
-		$tab = read('../sources_files/crocodile512.d');	
+		
+		$tab = read('../sources_files/herisson512.d');	
 	}
-
-  
-    $res= decomposition($tab);
-	$resultat = ['origin'=>$tab,'moyenne'=> array_values($res[1]), 'detail'=> array_values($res[0])];
+	//$detail=[];
+    $res= decomposition_totale($tab);
+	$resultat = ['origin'=>$tab,'moyenne'=> array_values($res[0]), 'detail'=> array_values($res[1])];
 	echo json_encode($resultat);
+	
 ?>
