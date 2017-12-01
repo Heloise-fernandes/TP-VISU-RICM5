a6d8df02af1b627290fa5c806e203602c9166454
douria <douria.zen@gmail.com>
2017-12-01 13:57:36 +0100

    exo 5

--- TP4_visu/Exercice5.py
+++ TP4_visu/Exercice5.py
@@ -19,6 +19,10 @@ listC = cities.split("\n")
 
 for i in range(len(listC)-1):
     listl=listC[i].split(" ")
+    if(i>len(listC)-1)
+         listlSuivant=listC[0].split(" ")
+    else   listlSuivant=listC[i+1].split(" ")
+
     kml+=('<Placemark>'
           '<name>'+listl[0]+ str(listl[3])+'</name>'
           '<TimeSpan>'
@@ -27,6 +31,13 @@ for i in range(len(listC)-1):
           '<Point>'
           '<coordinates>'+str(listl[1])+','+str(listl[2])+'</coordinates>'
           '</Point>'
+          '<LineString>'
+          '<styleUrl>#yellowLineGreenPoly</styleMode>'
+          '<extrude>1</extrude>'
+          '<tessellate>1</tessellate>'
+          '<altitudeMode>absolute</altitudeMode>'
+          '<coordinates>'+str(listl[1])+','+str(listl[2])+','+str(listlSuivant[1])+','+str(listlSuivant[2])+'</coordinates>'
+          '</LineString>'
           '</Placemark>'
     )
 
