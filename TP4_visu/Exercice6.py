8eda53b6f56c04a2d97f12cc627312dcff2cafc2
Heloise-fernandes <heloise.fernandesdealmeida@gmail.com>
2017-12-01 15:58:29 +0100

    Correction et test exo 6

--- TP4_visu/Exercice6.py
+++ TP4_visu/Exercice6.py
@@ -16,17 +16,17 @@ with open("cities.txt","r") as fichier:
     cities=fichier.read()
     
 listC = cities.split("\n")
-
+numStep = 10
 for i in range(len(listC)-1):
     listl=listC[i].split(" ")
     if(i>=len(listC)-2):
          listlSuivant=listC[0].split(" ")
     else  : listlSuivant=listC[i+1].split(" ")
 
-    diffLatitude=listlSuivant[1]-listl[1]
-    diffLongetude=listlSuivant[2]-listl[2]
-    pasLatitude=diffLatitude/10
-    pasLongetitude=diffLongetude/10
+    diffLatitude=float(listlSuivant[1])-float(listl[1])
+    diffLongetude=float(listlSuivant[2])-float(listl[2])
+    pasLatitude=diffLatitude/numStep
+    pasLongetitude=diffLongetude/numStep
     
     kml+=('<Placemark>'
           '<name>'+listl[0]+ str(listl[3])+'</name>'
@@ -43,14 +43,15 @@ for i in range(len(listC)-1):
           '<tessellate>1</tessellate>'
           '<altitudeMode>absolute</altitudeMode>'
           '<coordinates>')
+          
     DepartLat=listl[1]
     DepartLong=listl[2]
-    for j in 0..10:
-        kml+( str(DepartLat+j*pasLatitude)+','+str(DepartLong+j*pasLongetitude)+',100000\n')
-          
-    kml+('</coordinates>'
+    for j in range(numStep+1):
+        kml+=( str(float(DepartLat)+j*pasLatitude)+','+str(float(DepartLong)+j*pasLongetitude)+',100000\n')
+        
+    kml+=('</coordinates>'
           '</LineString>'
-          '</Placemark>'
+          '</Placemark>\n'
     )
 
 kml+=('</Document></kml>')
